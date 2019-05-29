365f8b122db6ada752d8d99713630a3100b8cae9
Mustafa ÖNCEL <support@lifemcserver.com>
2019-05-29 11:28:11 +0300

    Fix newly added warning always gives warning
    Signed-off-by: Mustafa ÖNCEL <support@lifemcserver.com>

--- src/main/java/ch/njol/skript/effects/EffMessage.java
+++ src/main/java/ch/njol/skript/effects/EffMessage.java
@@ -81,7 +81,7 @@ public final class EffMessage extends Effect {
         for (final String message : messages.getArray(e)) {
 //			message = StringUtils.fixCapitalization(message);
             final CommandSender[] recipientsArray = recipients.getArray(e);
-            if (SkriptConfig.enableExplicitPlayerUseWarnings.value() && (recipientsArray.length == 0 || (recipientsArray.length == 1 && !(recipientsArray[0] instanceof ConsoleCommandSender) && e instanceof ServerCommandEvent))) {
+            if (SkriptConfig.enableExplicitPlayerUseWarnings.value() && recipientsArray.length == 1 && !(recipientsArray[0] instanceof ConsoleCommandSender) && e instanceof ServerCommandEvent) {
                 Skript.warning("Command used from console, but send message uses the form of explicit \"to player\". For clarification, limit the command to the players, or remove the \"to player\" part." + (script != null ? " (" + script + ", line " + line + ")" : ""));
             }
             for (final CommandSender s : recipientsArray) {
