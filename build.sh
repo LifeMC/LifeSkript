bee9ddd0319055a55ed4f0d94ab600264d3c2f88
Mustafa ÖNCEL <support@lifemcserver.com>
2019-07-07 05:50:19 +0300

    Actually fix the build failing
    Signed-off-by: Mustafa ÖNCEL <support@lifemcserver.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+./mvnw install:install-file -Dfile=lib/timings-1.8.8-SNAPSHOT.jar -DgroupId=co.aikar -DartifactId=timings -Dversion=1.8.8-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/vault-1.5.6.jar -DgroupId=net.milkbowl.vault -DartifactId=vault -Dversion=1.5.6 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/ecj-4.12.jar -DgroupId=org.eclipse.jdt.core.compiler -DartifactId=ecj -Dversion=4.12 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/sqlibrary-7.1.jar -DgroupId=patpeter -DartifactId=sqlibrary -Dversion=7.1 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/griefprevention-13.9.1.jar -DgroupId=me.ryanhamshire -DartifactId=griefprevention -Dversion=13.9.1 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/worldguard-6.1.2.jar -DgroupId=com.sk89q -DartifactId=worldguard -Dversion=6.1.2 -Dpackaging=jar -DgeneratePom=true
+./mvnw install:install-file -Dfile=lib/worldedit-6.1.9.jar -DgroupId=com.sk89q -DartifactId=worldedit -Dversion=6.1.9 -Dpackaging=jar -DgeneratePom=true
+
 ./mvnw -Duser.name="Skript Team" -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -DcompilerArgument=-O -e -U clean install package
 java -Duser.name="Skript Team" -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -jar lib/proguard.jar @Skript.pro
