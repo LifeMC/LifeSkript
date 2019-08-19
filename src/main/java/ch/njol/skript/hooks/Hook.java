86e46ea94221b95e5dac32cfd27b0724bb01fc81
Mustafa ÖNCEL <support@lifemcserver.com>
2019-08-19 16:07:20 +0300

    Hook system improvements

--- src/main/java/ch/njol/skript/hooks/Hook.java
+++ src/main/java/ch/njol/skript/hooks/Hook.java
@@ -28,6 +28,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Peter Güttinger
@@ -36,7 +38,7 @@ public abstract class Hook<P extends Plugin> {
 
     public static final ArgsMessage m_hooked = new ArgsMessage("hooks.hooked"),
             m_hook_error = new ArgsMessage("hooks.error");
-
+    private static final Map<String, Hook<? extends Plugin>> hooks = new HashMap<>();
     public final P plugin;
 
     @SuppressWarnings("null")
@@ -45,12 +47,37 @@ public abstract class Hook<P extends Plugin> {
         plugin = p;
         if (p == null)
             return;
-        if (!init()) {
+        if (!Bukkit.getPluginManager().isPluginEnabled(p) || !init()) {
             Skript.error(m_hook_error.toString(p.getName()));
             return;
         }
         loadClasses();
         Skript.info(m_hooked.toString(p.getName()));
+
+        hooks.remove(p.getName());
+        hooks.put(p.getName(), this);
+    }
+
+    public static final boolean isHookEnabled(final Plugin p) {
+        return isHookEnabled(p.getName());
+    }
+
+    public static final boolean isHookEnabled(final String s) {
+        return hooks.containsKey(s);
+    }
+
+    public static final <T extends Plugin> Hook<T> getHook(final T p) {
+        return (Hook<T>) getHook(p.getName());
+    }
+
+    /**
+     * Preferably use {@link Hook#getHook(Plugin)} instead. It's more type-safe.
+     *
+     * @param s The name of the plugin.
+     * @return The hook instance for the given plugin, null if it does not exist.
+     */
+    public static final Hook<? extends Plugin> getHook(final String s) {
+        return hooks.get(s);
     }
 
     public final P getPlugin() {
