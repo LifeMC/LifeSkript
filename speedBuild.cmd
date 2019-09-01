10bc4b543fc543cd4c44cda9ee43b0736e84faee
Mustafa ÖNCEL <support@lifemcserver.com>
2019-09-01 04:19:23 +0300

    Show exception stack traces also on speed builds

--- speedBuild.cmd
+++ speedBuild.cmd
@@ -2,5 +2,5 @@
 chcp 65001 > nul
 @setlocal enableextensions
 @cd /d "%~dp0"
-call mvn install -Dversions.skip=true -Dmaven.test.skip=true -DskipTests=true -Dmaven.test.skip.exec=true -Dmaven.site.skip=true -Dmaven.javadoc.skip=true -P !deploy,!only-eclipse
+call mvn install -e -Dversions.skip=true -Dmaven.test.skip=true -DskipTests=true -Dmaven.test.skip.exec=true -Dmaven.site.skip=true -Dmaven.javadoc.skip=true -P !deploy,!only-eclipse
 pause
